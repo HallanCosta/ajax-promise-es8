6e860e12a19578466c5724b09b8c3e25cdbf00c4
HallanCosta <hallan.costa1@hotmail.com>
2020-03-25 18:42:56 -0300

    Ajax 1.0

---
+++ ajax.js
@@ -0,0 +1,65 @@
+
+const message = {
+
+	sigle: (element, message) => {
+		element.innerHTML = message;
+	},
+
+	multiples: (element, message, callback) => {
+		element.innerHTML = message;
+		callback();
+	}
+}
+
+const ajax = {
+
+	post: async (url, formData, notification=false) => {
+
+		const params =  new URLSearchParams([...new FormData(formData)]);
+
+		if (notification) {
+			console.log(`URL: ${url}.php \n FormData: ${params}`)
+		}
+
+		const myInit = {
+			method: 'POST',
+			body: params
+		}
+
+		const response = await fetch(url + '.php', myInit);
+
+		const data = await response.text();
+
+		if (notification) {
+			window.alert(`Status: ${response.status} \n StatusText: ${response.statusText}`);
+		}
+
+		return data;
+	},
+
+	get: async (url, parameters='', notification=false) => {
+
+		if (notification) {
+			console.log(`URL: ${url}.php`)
+			console.log(`Parameters: ${parameters}`)
+			console.log('Two all: ' + url + '.php?' + parameters)
+		}
+
+		const myInit = {
+			method: 'GET'
+		}
+
+		const response = await fetch(url + '.php?' + parameters, myInit);
+
+		const data = await response.text();
+
+		if (notification) {
+			window.alert(`Status: ${response.status} \n StatusText: ${response.statusText}`);
+		}
+
+		return data;
+
+	}
+
+}
+
